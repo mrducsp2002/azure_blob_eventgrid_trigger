ee8eb7b940b327b827703f2478abc45a53b21f6a
Infernope <scott.xu220@gmail.com>
2026-04-20 14:35:36 +1000

    Fixed studentID extraction. Previously was extracting the assignment file name instead of the folder name above (which is the studentID)

--- src/processor.py
+++ src/processor.py
@@ -56,8 +56,7 @@ def _process_zip_file(zip_bytes: bytes, blob_name: str, collection):
             if len(parts) < 2:
                 continue
 
-            student_id_raw = parts[1].split('-')[0].strip().lower()  # Extract student ID
-            student_id = os.path.splitext(student_id_raw)[0]
+            student_id = parts[0].split('-')[0].strip().lower()  # Extract student ID
             file_name = parts[-1]
 
             try:
